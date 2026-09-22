4907bffba3d6a04a4e891b58bb08998fb55bdd2f
Monika R K <monikarkmonu@gmail.com>
2026-09-22 21:28:56 +0530

    Time: 37 ms (7.65%), Space: 81.2 MB (98.21%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
@@ -4,12 +4,11 @@ class Solution {
         HashMap<Integer,Integer> m=new HashMap<>();
         long count=0;
         int sum=0;
-        
+
         m.put(0,1);
         for(int i=0;i<arr.length;i++)
         {
             sum+=arr[i];
-           
             if(sum%2==1)
             {
                 count+=(long)m.getOrDefault(0,0);
@@ -18,9 +17,8 @@ class Solution {
             {
                 count+=(long)m.getOrDefault(1,0);
             }
-            
+
             m.put(sum%2,m.getOrDefault(sum%2,0)+1);
-           
         }
         return (int)(count%1000000007);
     }
