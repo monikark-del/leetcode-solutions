30bc52be47ce0492eab7300741f096e203e2ce0a
Monika R K <monikarkmonu@gmail.com>
2026-09-22 21:14:30 +0530

    Time: 37 ms (7.65%), Space: 81.4 MB (94.64%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        
+        HashMap<Integer,Integer> m=new HashMap<>();
+        long count=0;
+        int sum=0;
+        
+        m.put(0,1);
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+           
+            if(sum%2==1)
+            {
+                count+=(long)m.getOrDefault(0,0);
+            }
+            else
+            {
+                count+=(long)m.getOrDefault(1,0);
+            }
+            
+            m.put(sum%2,m.getOrDefault(sum%2,0)+1);
+           
+        }
+        return (int)(count%1000000007);
+    }
+}
