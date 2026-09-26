e5af96037bfebf5fc3620b7e0a11b417b8d56f83
Monika R K <monikarkmonu@gmail.com>
2026-09-26 21:46:11 +0530

    Time: 0 ms (100%), Space: 48.5 MB (30.78%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -6,7 +6,7 @@ class Solution {
 
         while(low <=high)
         {
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target)
             {
                 return mid;
